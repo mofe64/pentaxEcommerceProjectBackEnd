d4becfdcc32094cb2732d9f767bc932b02f3f4b2
Audu Billy <audubilly89@gmail.com>
2021-04-07 09:24:29 +0100

    Controller updates

--- src/main/java/com/pentax/pentazon/security/UnauthorizedEntryPoint.java
+++ src/main/java/com/pentax/pentazon/security/UnauthorizedEntryPoint.java
@@ -13,7 +13,8 @@ import java.io.Serializable;
 @Component
 public class UnauthorizedEntryPoint implements AuthenticationEntryPoint, Serializable {
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
         httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
