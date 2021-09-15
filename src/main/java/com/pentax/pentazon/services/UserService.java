53ba7f8813b4858a111d66a2de320e2c5da23acb
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-09-15 16:10:21 +0100

    updated db uri

--- src/main/java/com/pentax/pentazon/services/UserService.java
+++ src/main/java/com/pentax/pentazon/services/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service(value = "userService")
 public interface UserService {
     UserDTO registerBuyer(UserDTO buyerDetails) throws UserException;
 
